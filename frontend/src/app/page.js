f4bae94cbc0e5945e7a6d6451cc87a8d730c6e55
SukhpreetO1 <ssingh77022@gmail.com>
2025-03-20 18:33:46 +0530

    create 2 folder - frontend, backend & add nestjs

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+import { VideoPlayer, Navbar } from "@/routes/page";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <VideoPlayer />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/routes/page.jsx
@@ -0,0 +1,5 @@
+export { Input } from "@/components/ui/input.jsx";
+export { Button } from "@/components/ui/button.jsx";
+export { ThemeProvider } from "@/components/(common)/theme_provider/page.jsx";
+export { default as VideoPlayer } from "@/components/VideoPlayer/page.jsx";
+export { default as Navbar } from "@/components/(common)/(navbar)/page.jsx";
